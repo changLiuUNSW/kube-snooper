138f565556ca46b866fabe12f5fbf3565a4e1d1b
chang liu <3dslayer@gmail.com>
2018-06-20 13:02:41 +1000

    update heading

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,7 @@ const Nav = () => (
   <nav className="navbar navbar-inverse navbar-static-top">
     <div className="container">
       <div className="navbar-header">
-        <div className="navbar-brand">Minecraft Kube Snooper</div>
+        <div className="navbar-brand">Minecraft Kube Snooper - Team 4</div>
       </div>
     </div>
   </nav>
